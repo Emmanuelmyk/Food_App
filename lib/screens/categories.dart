007a766532b2280ca3d09ed3295365237ab83bf5
Emmanuel Mbah <emmanuelmbah91@gmail.com>
2024-09-28 16:58:59 +0100

    screen navigation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:mealz/data/dummy_data.dart';
 import 'package:mealz/models/category.dart';
+import 'package:mealz/screens/meals.dart';
 import 'package:mealz/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (ctx) => MealsScreen(title: 'some title', meals: [])),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Food Categories'),
+        title: Text('Pick your Categories'),
       ),
       body: GridView(
         padding: EdgeInsets.all(24),
@@ -22,7 +31,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           //availableCategories.map((category)=> CategoryGridItem(category:)).toList();
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
